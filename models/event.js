b44bbd5c428c6e6a9220e739e9601986e031d2e6
Austin Keener <austingraphicandweb@gmail.com>
2020-07-16 18:53:34 -0400

    boiler plate added to login.js and signup.js/lowercase to objects in event.js model and fk syntax

--- models/event.js
+++ models/event.js
@@ -1,22 +1,22 @@
 module.exports = function (sequelize,DataTypes){
     const Event = sequelize.define("Event",{
-        Title: {
+        title: {
             type: DataTypes.STRING
         },
-        Dates: {
+        date: {
             type: DataTypes.STRING
         },
-        Description: {
+        description: {
             type: DataTypes.STRING
         },
-        Location: {
+        location: {
             type: DataTypes.STRING
         }
     })
     // the associate property is to connect two tables together.
     Event.associate = function(model){
         Event.belongsTo(model.Event , {
-            foreignKey: "EventID"
+            foreignKey: "EventId"
         })
     }
     return Event;
