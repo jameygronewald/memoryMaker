cc21a133f61562bab81ffe5dfa563c9a05bcbd92
Jared Porter <jaredp17@me.com>
2020-07-17 15:29:03 -0400

    Missing comma fix

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "root"
+    "username": "root",
     "password": process.env.PASSWORD,
     "database": process.env.DATABASE,
     "host": "localhost",
