2486eaa6297344808f1a421f3fad947555b3c6bf
Jared Porter <jaredp17@me.com>
2020-07-18 05:06:47 -0400

    On click button methods for href routes.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+  const signUpBtn = $("#sign-up");
+  const loginBtn = $("#login");
 
+  signUpBtn.on("click", (res, err) => {
+    if (err) throw err;
+    window.location.href = "signUp";
+  });
 
+  loginBtn.on("click", (res, err) => {
+    if (err) throw err;
+    window.location.href = "login";
+  });
 });
