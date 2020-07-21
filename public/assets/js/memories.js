fe9e26914dbf923c24368566e68ffe1f4eacc657
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-21 11:06:39 -0700

    reformatted image thumbnails on memories page, added logout functionality to main.js and script tag to main.handlebars to create a globally functioning logout feature

--- public/assets/js/memories.js
+++ public/assets/js/memories.js
@@ -1,11 +1,4 @@
 $(document).ready(() => {
-    const logoutButton = $('#logoutButton');
-    $(logoutButton).on('click', function(event) {
-        event.preventDefault();
-        localStorage.clear();
-        document.cookie = 'sessionToken=; expires=Thu, 20 Aug 2016 00:00:00 UTC; path=/;'
-        window.location.href = '/';
-    });
     const createButton = $('#createButton');
     $(createButton).on('click', function(event) {
         event.preventDefault();
