10c80d05ed03e773cc505062db6aaa360f472627
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-21 07:53:01 -0700

    added image data and username data to single memory object returned from query when viewing a single memory; cleaned up some old commented out code in newMemory.js

--- public/assets/js/memories.js
+++ public/assets/js/memories.js
@@ -5,16 +5,16 @@ $(document).ready(() => {
         localStorage.clear();
         document.cookie = 'sessionToken=; expires=Thu, 20 Aug 2016 00:00:00 UTC; path=/;'
         window.location.href = '/';
-    })
+    });
     const createButton = $('#createButton');
     $(createButton).on('click', function(event) {
         event.preventDefault();
         window.location.href = '/memories/newMemory';
-    })
+    });
     const viewButton = $('.viewButton');
     $(viewButton).on('click', function(event) {
         event.preventDefault();
         const memoryId = $(this).attr('data-id');
         window.location.href = `/memories/id/${memoryId}`;
-    })
-})
+    });
+});
--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
--- Drops the todolist if it exists currently --
 DROP DATABASE IF EXISTS memories_db;
--- Creates the "todolist" database --
+
 CREATE DATABASE memories_db;
