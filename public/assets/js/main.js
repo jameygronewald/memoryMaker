0ca135976910b5e132f0f33a0f8ef7f0bf4a2360
Jared Porter <jaredp17@me.com>
2020-07-21 21:24:23 -0400

    Refactor go back button.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
-    const logoutButton = $('#logoutButton');
-    $(logoutButton).on('click', function(event) {
-        event.preventDefault();
-        localStorage.clear();
-        document.cookie = 'sessionToken=; expires=Thu, 20 Aug 2016 00:00:00 UTC; path=/;'
-        window.location.href = '/';
-    });
+  const logoutButton = $("#logoutButton");
+  const goBack = $("#goBack");
+  
+  $(logoutButton).on("click", function (event) {
+    event.preventDefault();
+    localStorage.clear();
+    document.cookie =
+      "sessionToken=; expires=Thu, 20 Aug 2016 00:00:00 UTC; path=/;";
+    window.location.href = "/";
+  });
+
+  
+  goBack.on("click", () => {
+    window.location.replace("/memories");
+  });
 });
